c1147b7a096b02e19c8a047797294ddaee413d57
John <ristaujo@gmail.com>
2017-09-14 18:18:45 -0400

    Add inverse_of to all build has_many associations

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,12 +1,9 @@
 class Build < ApplicationRecord
-  has_many :build_talents, dependent: :destroy
+  has_many :build_talents, inverse_of: :build, dependent: :destroy
   has_many :talents, through: :build_talents
 
-  has_many :build_maps, dependent: :destroy
+  has_many :build_maps, inverse_of: :build, dependent: :destroy
   has_many :maps, through: :build_maps
-
-  has_many :build_heros, inverse_of: :build, dependent: :destroy
-  has_many :synergies
 
   has_many :counters, inverse_of: :build
   has_many :counter_heros, through: :counters, source: :hero
@@ -18,7 +15,6 @@ class Build < ApplicationRecord
 
   accepts_nested_attributes_for :build_talents
   accepts_nested_attributes_for :build_maps
-  accepts_nested_attributes_for :build_heros
   accepts_nested_attributes_for :counters
   accepts_nested_attributes_for :synergies
 end
