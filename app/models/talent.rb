bb6624909ce347f427089ca39e464d06a08fd5fe
John <ristaujo@gmail.com>
2017-09-14 14:13:46 -0400

    Updated talent.rb with correct nested attributes

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -5,5 +5,6 @@ class Talent < ApplicationRecord
   has_many :build_talents
   has_many :builds, through: :build_talents
   
-  accepts_nested_attributes_for :hero_talents, :build_talents
+  accepts_nested_attributes_for :hero_talents
+  accepts_nested_attributes_for :build_talents
 end
