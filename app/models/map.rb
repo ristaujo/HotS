08f39ca26bd8e92ae4db543efcc1c3845d0d098e
John <ristaujo@gmail.com>
2017-09-14 14:01:53 -0400

    Updated talent and map models

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,2 +1,4 @@
 class Map < ApplicationRecord
+  has_many :build_maps
+  has_many :builds, through: :build_maps
 end
--- app/models/talent.rb
+++ app/models/talent.rb
@@ -1,6 +1,9 @@
 class Talent < ApplicationRecord
   has_many :hero_talents
   has_many :heros, through: :hero_talents
+
+  has_many :build_talents
+  has_many :builds, through: :build_talents
   
-  accepts_nested_attributes_for :hero_talents
+  accepts_nested_attributes_for :hero_talents, :build_talents
 end
